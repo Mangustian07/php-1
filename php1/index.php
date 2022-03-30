0cfdc29a06035e1adbb6200d790fe1fd10d5a7f4
Daulet <99522956+Mangustian07@users.noreply.github.com>
2022-03-30 19:53:31 +0600

    Add files via upload

---
+++ php1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1-zadanie</title>
+</head>
+
+<body>
+    <?php
+    $a = 45;
+    $b = 25;
+    $c = $a - $b;
+    $d = 7;
+    $result = $c + $d;
+    echo "Результат: ", $result;
+    ?>
+</body>
+
+</html>
